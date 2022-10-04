00395138968732e66cdaa73a45d37c908432358a
Jay Joo <jj534@cornell.edu>
2022-10-04 15:42:30 -0400

    improve give referral page

--- pages/give-referrals.tsx
+++ pages/give-referrals.tsx
@@ -1,22 +1,28 @@
 import RequestList from "components/give-referral/RequestList"
 import Flex from "components/glue/Flex"
-import GlueSplitScreen from "components/glue/SplitScreen"
+import SplitScreen from "components/glue/SplitScreen"
 import PageContainer from "components/glue/PageContainer"
 import { useSession } from "next-auth/react"
 import React from "react"
 import Button from "components/glue/Button"
 import Link from "next/link"
+import Image from "next/image"
+import { Space } from "@mantine/core"
+import GlueResponsiveRender from "components/glue/GlueResponsiveRender"
+import useIsMobile from "hooks/glue/isMobile"
 
-const GiveReferralPage = () => {
+const GiveReferralsPage = () => {
   const { status } = useSession()
+  const isMobile = useIsMobile()
 
   if (status === "authenticated") {
     return <RequestList />
   }
 
   return (
-    <PageContainer variant="mobile-only">
-      <GlueSplitScreen
+    <PageContainer variant="responsive">
+      <Space my={isMobile ? "3rem" : "5rem"} />
+      <SplitScreen
         title="Start giving referrals"
         paragraphs={[
           "Sign in with your company email to start receiving referral requests anonymously.",
@@ -29,9 +35,17 @@ const GiveReferralPage = () => {
             </Link>
           </Flex>
         }
+        illust={
+          <Image
+            src="/illust/professors.svg"
+            alt="professor illustration"
+            width={340}
+            height={340}
+          />
+        }
       />
     </PageContainer>
   )
 }
 
-export default GiveReferralPage
+export default GiveReferralsPage
