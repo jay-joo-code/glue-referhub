7a0842c59775e98729980739684e3caa318b8b2c
Jay Joo <jj534@cornell.edu>
2022-10-04 15:25:23 -0400

    add request list component

---
+++ components/give-referral/RequestList.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const RequestList = () => {
+  // TODO: get company list by user domain
+  // if company domain not in DB,
+  // enter
+  return <div>RequestList</div>
+}
+
+export default RequestList
