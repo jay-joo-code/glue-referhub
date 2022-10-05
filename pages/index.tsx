b07e0104f6f46dc81094909f1596aabe664d4de3
Jay Joo <jj534@cornell.edu>
2022-10-04 21:33:28 -0400

    implement landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,35 @@
-import React from "react"
+import { Space } from "@mantine/core"
+import Button from "components/glue/Button"
+import PageContainer from "components/glue/PageContainer"
+import SplitScreen from "components/glue/SplitScreen"
+import useIsDevice from "hooks/glue/useIsDevice"
 
 const Index = () => {
-  return <div>Index</div>
+  const { isMobile } = useIsDevice()
+
+  return (
+    <PageContainer title="Job referrals at Cornell" variant="responsive">
+      <Space my={isMobile ? "3rem" : "5rem"} />
+      <SplitScreen
+        title="Job referrals, simplified"
+        paragraphs={[
+          "ReferHub is matches students looking for job referrals with alums who are actively looking to give referrals 🙌",
+          "Sign up for early access to try out ReferHub to get job referrals November 2022",
+        ]}
+        buttons={[
+          <a
+            key="request-early-access"
+            href="https://tinyurl.com/referhub-student-early"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button size="md">Request early access</Button>
+          </a>,
+        ]}
+        illustPath="/illust/landing.svg"
+      />
+    </PageContainer>
+  )
 }
 
 export default Index
